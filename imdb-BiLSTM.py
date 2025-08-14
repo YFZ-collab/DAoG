226bbb1f2ebc81e41e63a05423a29cccdbf82f7d
YFZ-collab <a852702291@gmail.com>
2025-08-14 18:23:50 +0800

    imdb-BiLSTM.py

--- imdb-BiLSTM.py
+++ imdb-BiLSTM.py
@@ -154,7 +154,7 @@ class DoG(optimizers.Optimizer):
 
       tf.cond(
           condition,
-          #lambda: K.update(m, 1e-4*(tf.norm(ini)+1)),
+          #lambda: K.update(m, 1e-6*(tf.norm(ini)+1)),
           lambda: K.update(m, 1e-1*(tf.norm(ini)+1)),
           lambda: K.update(m, tf.maximum(m, tf.norm(var - ini))),
       )
@@ -419,4 +419,5 @@ plt.legend()
 plt.grid(True)
 
 plt.tight_layout()
+
 plt.show()
